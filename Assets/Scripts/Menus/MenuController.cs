91c778e0c07c4e8fbf4393ce31e080f87c857f77
Jordi <xbarciap@gmail.com>
2022-07-09 11:45:56 +0200

    Menus

---
+++ Assets/Scripts/Menus/MenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+	[SerializeField] GameObject PauseMenu;
+	[SerializeField] GameObject RestartMenu;
+	[SerializeField] GameObject StartScreen;
+	private void Update()
+	{
+		if (PauseMenu.activeSelf || RestartMenu.activeSelf || StartScreen.activeSelf)
+		{
+			Time.timeScale = 0f;
+		}
+		else { Time.timeScale = 1f; }
+	}
+}
